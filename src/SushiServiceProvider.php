23d121787e0fa3dbb83843d2fc958e3fd6dcc844
Ryan Chandler <ryan@chandler.codes>
2020-03-04 14:59:04 +0000

    Added publishable config file

--- src/SushiServiceProvider.php
+++ src/SushiServiceProvider.php
@@ -6,5 +6,17 @@ use Illuminate\Support\ServiceProvider;
 
 class SushiServiceProvider extends ServiceProvider
 {
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/../config/sushi.php' => config_path('sushi'),
+        ], 'config');
+    }
 
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__.'/../config/sushi.php', 'sushi'
+        );
+    }
 }
